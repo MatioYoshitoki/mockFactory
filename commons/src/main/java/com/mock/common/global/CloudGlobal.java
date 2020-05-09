9be2d5bf32938923e6b9e2056686a61c1f8e60b9
柳挺 <liuting_502@126.com>
2020-05-09 16:22:20 +0800

    1.删除key

--- commons/src/main/java/com/mock/common/global/CloudGlobal.java
+++ commons/src/main/java/com/mock/common/global/CloudGlobal.java
@@ -10,12 +10,6 @@ public class CloudGlobal {
 
 	public static final String SIGN_NAME="MockFactory";
 
-	/*阿里云短信账号*/
-	public static String ACCESS_KEY_ID = "";
-
-	/*阿里云短信密码*/
-	public static String SECRET = "";
-
 	public static final String TOKEN = "token";
 
 	public static final String USER_PO = "userPo";
